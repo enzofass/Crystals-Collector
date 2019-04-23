3151ef40076651cb3f102a79ac70a51774df4983
enzofass <zoefassioli@gmail.com>
2019-04-22 23:24:49 -0400

    added js to functional game, still need to fine tune and clean up program

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,100 @@
 // game variables
-let redClicked = false;
-let blueClicked = false;
-let yellowClicked = false;
-let greenClicked = false;
 let redCrystalValue = 0;
 let blueCrystalValue = 0;
 let yellowCrystalValue = 0;
 let greenCrystalValue = 0;
 let playerNumSum = 0;
-let MagicNum = 0;
-
-// click listeners
-$("#red-crystal").on("click", function() {
-  console.log("red click pressed");
-  redClicked = true;
-  getRandomNum()
-});
-
-$("#blue-crystal").on("click", function() {
-  console.log("blue click pressed");
-  blueClicked = true;
-});
-
-$("#yellow-crystal").on("click", function() {
-  console.log("yellow click pressed");
-  yellowClicked = true;
-});
-
-$("#green-crystal").on("click", function() {
-  console.log("green click pressed");
-  greenClicked = true;
-});
+let magicNum = 0;
+let totalWin = 0;
+let totalLoss = 0;
+let newGame = false;
 
 // function to generate randaom number
-function getRandomNum(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 // function to add crystal number to players number
 function addCrystalNum(crystalNum, playerNum) {
   return crystalNum + playerNum;
-
 }
 
-for (let i=0; )
-switch ((redClicked, blueClicked, yellowClicked, greenClicked)) {
-  case redClicked:
-    text = "Off";
-    break;
-  case blueClicked:
-    text = "On";
-    break;
-  case yellowClicked:
-    text = "On";
-    break;
-  case greenClicked:
-    text = "On";
-    break;
-  default:
-    text = "No value found";
+function restartGame() {
+  // if (playerNumSum === 0 || newGame) {
+  magicNum = getRandomNum(1, 100);
+  $("#rand-num").html(magicNum);
+  playerNumSum = 0;
+  redCrystalValue = getRandomNum(1, 12);
+  blueCrystalValue = getRandomNum(1, 12);
+  yellowCrystalValue = getRandomNum(1, 12);
+  greenCrystalValue = getRandomNum(1, 12);
+  newGame = false;
+}
+
+function checkScore() {
+  console.log(playerNumSum, magicNum);
+  if (playerNumSum > magicNum) {
+    // alert("You Lost!");
+    totalLoss++;
+    $("#losses").html(totalLoss);
+    restartGame();
+  } else if (playerNumSum === magicNum) {
+    // alert("You WIn!");
+    totalWin++;
+    $("#wins").html(totalWin);
+    restartGame();
+  } else {
+  }
+}
+// code trying to make it so that crytal values are different
+// }if (redCrystalValue === blueCrystalValue) {
+//   blueCrystalValue = getRandomNum(1, 12);
+// }if (blueCrystalValue != redCrystalValue) {
+//   yellowCrystalValue = getRandomNum(1, 12);
+// }if (yellowCrystalValue != blueCrystalValue){
+//   greenCrystalValue = getRandomNum(1, 12);
+// }else{
+//   yellowCrystalValue = getRandomNum(1, 12);
+//   greenCrystalValue = getRandomNum(1, 12);
+// }
+restartGame();
+// click listeners
+$(".crystals").on("click", function(event) {
+  console.log("crystal clicked", $(this).data("crystal"));
+  switchCaseFunc($(this).data("crystal"));
+});
+
+function switchCaseFunc(color) {
+  switch (color) {
+    case "red":
+      console.log("Red case init", redCrystalValue);
+      // addCrystalNum(redCrystalValue, playerNumSum);
+      playerNumSum = playerNumSum + redCrystalValue;
+      $("#play-num").html(playerNumSum);
+      checkScore();
+
+      break;
+    case "blue":
+      console.log("Blue case init", blueCrystalValue);
+      playerNumSum = playerNumSum + blueCrystalValue;
+      $("#play-num").html(playerNumSum);
+      checkScore();
+
+      break;
+    case "yellow":
+      console.log("Yellow case init", yellowCrystalValue);
+      playerNumSum = playerNumSum + yellowCrystalValue;
+      $("#play-num").html(playerNumSum);
+      checkScore();
+
+      break;
+    case "green":
+      console.log("Green case init", greenCrystalValue);
+      playerNumSum = playerNumSum + greenCrystalValue;
+      $("#play-num").html(playerNumSum);
+      checkScore();
+
+      break;
+    default:
+      text = "No value found";
+  }
 }
